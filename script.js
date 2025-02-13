d3936e2f5ab7b857218966382c6868b00da71946
Shinnosuke-Nakayama <nakayama.shinnosuke@outlook.jp>
2025-02-13 20:17:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,54 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
 
-const start = document.getElementById('start'); // 開始ボタン
+const start = document.getElementById("start"); // 開始ボタン
+const standby = document.getElementById("standby");//スタンバイボタン
+const cars = document.getElementById("cars");
+const carLineup = ["コンバインハーベスター", "トゥクトゥク", "ハイエース", "高所作業車", "消防車"];
+const steps = ["steps(100,start)", "steps(50,start)", "steps(25,start)", "steps(10,start)", "steps(5,start)"];
 
-const OK = document.getElementById('ok');
+function intRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-let memver = document.getElementById('member');
-
-const pattern = ['','コンバインハーベスター','トゥクトゥク','ハイエース','高所作業車','消防車'];
-
-const step = ['steps(100,start)','steps(50,start)','steps(25,start)','steps(10,start)','steps(5,start)']
-
-
-OK.addEventListener('click',()=>{
-  
-  let b = document.getElementById('num');
-  let c = b.value;
-
-   /** 重複チェック用配列 */
-var randoms = [];
-/** 最小値と最大値 */
-var min = 1, max = c;
-
-/** 重複チェックしながら乱数作成 */
-for(i = min; i <= max; i++){
-  while(true){
-    var tmp = intRandom(min, max);
-    if(!randoms.includes(tmp)){
-      randoms.push(tmp);
-      break;
+function numOfRandomMaker(min, max) {
+  const result = [];
+  for(let i = min; i <= max; i++) {
+    while(true) {
+      let randomNum = intRandom(min, max);
+      if(!result.includes(randomNum)) {
+        result.push(randomNum);
+        break;
+      }
     }
   }
-};
- 
-/** min以上max以下の整数値の乱数を返す */
-function intRandom(min, max){
-  return Math.floor( Math.random() * (max - min + 1)) + min;
-};
+  return result;
+}
 
-
-  for(var i = min; i <= c; i++){
-    let y = i-1;
-    let x = randoms[y]
-
-    memver.insertAdjacentHTML('beforeend',`<p><img id="image" src="https://raw.githubusercontent.com/Shinnosuke-Nakayama/Shinnosuke-Nakayama.fnd34/refs/heads/main/${pattern[x]}.png" width="200" height="150" alt="site-logo"></p>`);
-    // console.log(y);
-    console.log(x);
-
+standby.addEventListener("click", () => {
+  const arrayOfRandomNum = numOfRandomMaker(0, carLineup.length - 1);
+  for(const randomNum of arrayOfRandomNum) {
+    cars.insertAdjacentHTML("beforeend", `<p><img class="image" src="https://raw.githubusercontent.com/Shinnosuke-Nakayama/Shinnosuke-Nakayama.fnd34/refs/heads/main/${carLineup[randomNum]}.png" alt="site-logo">${carLineup[randomNum]}</p>`);
   }
-
 }
 );
 
- 
-// スタートボタンをクリックしたら
-start.addEventListener('click', () => {
-  let image = document.querySelectorAll("#image"); // ロゴ画像
-  let a = image.length
-
-  /** 重複チェック用配列 */
-var randoms = [];
-/** 最小値と最大値 */
-var min = 1, max = a;
-
-/** 重複チェックしながら乱数作成 */
-for(i = min; i <= max; i++){
-  while(true){
-    var tmp = intRandom(min, max);
-    if(!randoms.includes(tmp)){
-      randoms.push(tmp);
-      break;
-    }
+start.addEventListener("click", () => {
+  let images = document.querySelectorAll(".image");
+  const arrayOfRandomNum = numOfRandomMaker(1, images.length);
+  images.forEach((image,index) => {
+    image.animate(
+      [
+        { transform: "translateX(0)"}, 
+        { transform: "translateX(2000px)"}
+      ], 
+      {
+        fill: "backwards", 
+        duration: 5000 + (arrayOfRandomNum[index] * 500),
+        easing:steps[arrayOfRandomNum[index] - 1]
+      },
+    );
   }
-}
- 
-/** min以上max以下の整数値の乱数を返す */
-function intRandom(min, max){
-  return Math.floor( Math.random() * (max - min + 1)) + min;
-}
-
-
-  // 画像を左端から200px右に動かす
-  image.forEach((image,I)=> {
-  image.animate(
-    // 途中の状態を表す配列
-    [
-      { transform: 'translateX(0)'}, // 開始時の状態（左端）
-      { transform: 'translateX(2000px)' } // 終了時の状態（左端から200pxの位置）
-    ], 
-    // タイミングに関する設定
-    {
-      fill: 'backwards', // 再生前後の状態（再生前、開始時の状態を適用）
-      duration: 5000+(randoms[I]*500), // 再生時間（1000ミリ秒）
-      easing:step[I]
-    },
   );
 }
-  );
-});
+);
