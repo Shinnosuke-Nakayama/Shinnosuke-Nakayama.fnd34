bd4f18f21cf8477bdc185f9a4e02c36b84f47c5f
Shinnosuke-Nakayama <nakayama.shinnosuke@outlook.jp>
2025-02-16 09:40:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
-const elA = document.querySelector(".A");
-const elB = document.querySelector(".B");
-const iflame1 = document.querySelector(".upper");
-const iflame2 = document.querySelector(".lower");
+const leftWindow = document.querySelector(".leftWindow");
+const rightWindow = document.querySelector(".rightWindow");
+const iframeUpper = document.querySelector(".upper");
+const iframeLower = document.querySelector(".lower");
 const btn = document.querySelectorAll(".btn");
 const upperRight = document.querySelector(".upperRight");
 const lowerRight = document.querySelector(".lowerRight");
 
 function focus(event) {
-  if (event.target.id === "1") {
-    elA.style.width = "100%";
-    elB.style.display = "none";
+  if (event.target.id === "zoomOne") {
+    leftWindow.style.width = "100%";
+    rightWindow.style.display = "none";
 
-  } else if (event.target.id === "2") {
-      iflame2.style.display = "none"
-      WindowGetClikc3.style.visibility = "hidden";
-      WindowGetClikc6.style.visibility = "hidden";
-      iflame1.style.height = "100%";
+  } else if (event.target.id === "zoomTwo") {
+      iframeLower.style.display = "none"
+      WindowGetClikcZoomThree.style.visibility = "hidden";
+      WindowGetClikcReturenThree.style.visibility = "hidden";
+      iframeUpper.style.height = "100%";
       upperRight.style.height = "95%";
 
-  } else if (event.target.id === "3") {
-      iflame1.style.display = "none"
-      WindowGetClikc2.style.visibility = "hidden";
-      WindowGetClikc5.style.visibility = "hidden";
-      iflame2.style.height = "100%";
+  } else if (event.target.id === "zoomThree") {
+      iframeUpper.style.display = "none"
+      WindowGetClikcZoomTwo.style.visibility = "hidden";
+      WindowGetClikcReturnTwo.style.visibility = "hidden";
+      iframeLower.style.height = "100%";
       lowerRight.style.height = "95%";
   }
 }
 
 function revert(event) {
-  if (event.target.id === "A") {
-    elA.style.width = "50%"
-    elB.style.width = "50%";
-    elB.style.display = "block"
+  if (event.target.id === "returnOne") {
+    leftWindow.style.width = "50%"
+    rightWindow.style.width = "50%";
+    rightWindow.style.display = "block"
 
-  } else if (event.target.id === "B") {
-      elA.style.width = "50%"
-      elB.style.width = "50%";
-      iflame1.style.height = "50%";
-      elA.style.display = "block"
-      iflame2.style.display = "block"
-      WindowGetClikc3.style.visibility = "visible";
-      WindowGetClikc6.style.visibility = "visible";
+  } else if (event.target.id === "returnTwo") {
+      leftWindow.style.width = "50%"
+      rightWindow.style.width = "50%";
+      iframeUpper.style.height = "50%";
+      leftWindow.style.display = "block"
+      iframeLower.style.display = "block"
+      WindowGetClikcZoomThree.style.visibility = "visible";
+      WindowGetClikcReturenThree.style.visibility = "visible";
       upperRight.style.height = "85%"
 
-  } else if (event.target.id === "C") {
-      elA.style.width = "50%"
-      elB.style.width = "50%";
-      iflame2.style.height = "50%";
-      elA.style.display = "block"
-      iflame1.style.display = "block"
-      WindowGetClikc2.style.visibility = "visible";
-      WindowGetClikc5.style.visibility = "visible";
+  } else if (event.target.id === "returnThree") {
+      leftWindow.style.width = "50%"
+      rightWindow.style.width = "50%";
+      iframeLower.style.height = "50%";
+      leftWindow.style.display = "block"
+      iframeUpper.style.display = "block"
+      WindowGetClikcZoomTwo.style.visibility = "visible";
+      WindowGetClikcReturnTwo.style.visibility = "visible";
       lowerRight.style.height = "90%";
   }
 }
 
-const WindowGetClikc1 = document.querySelectorAll('input')[0];
-const WindowGetClikc2 = document.querySelectorAll('input')[2];
-const WindowGetClikc3 = document.querySelectorAll('input')[4];
+const WindowGetClikcZoomOne = document.querySelectorAll('input')[0];
+const WindowGetClikcZoomTwo = document.querySelectorAll('input')[2];
+const WindowGetClikcZoomThree = document.querySelectorAll('input')[4];
 
-const WindowGetClikc4 = document.querySelectorAll('input')[1];
-const WindowGetClikc5 = document.querySelectorAll('input')[3];
-const WindowGetClikc6 = document.querySelectorAll('input')[5];
+const WindowGetClikcReturnOne = document.querySelectorAll('input')[1];
+const WindowGetClikcReturnTwo = document.querySelectorAll('input')[3];
+const WindowGetClikcReturenThree = document.querySelectorAll('input')[5];
 
-WindowGetClikc1.addEventListener("click",focus);
-WindowGetClikc2.addEventListener("click",focus);
-WindowGetClikc3.addEventListener("click",focus);
+WindowGetClikcZoomOne.addEventListener("click",focus);
+WindowGetClikcZoomTwo.addEventListener("click",focus);
+WindowGetClikcZoomThree.addEventListener("click",focus);
 
-WindowGetClikc4.addEventListener("click",revert);
-WindowGetClikc5.addEventListener("click",revert);
-WindowGetClikc6.addEventListener("click",revert);
+WindowGetClikcReturnOne.addEventListener("click",revert);
+WindowGetClikcReturnTwo.addEventListener("click",revert);
+WindowGetClikcReturenThree.addEventListener("click",revert);
